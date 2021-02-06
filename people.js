4c7f593092c0ff657fc94d19dc7ae39cdd38f17f
ar4jun1 <arjun@toobler.com>
2021-02-06 15:13:50 +0530

    Create people.js

---
+++ people.js
@@ -0,0 +1,74 @@
+{
+  "people": [
+    {
+      "id": 0,
+      "isActive": false,
+      "picture": "http://placehold.it/32x32",
+      "name": "Miles Greer",
+      "email": "milesgreer@limage.com",
+      "phone": "+1 (949) 580-2360",
+      "greeting": "Hello, Miles Greer! You have 9 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "id": 1,
+      "isActive": false,
+      "picture": "http://placehold.it/32x32",
+      "name": "Hamilton Moreno",
+      "email": "hamiltonmoreno@limage.com",
+      "phone": "+1 (866) 551-2970",
+      "greeting": "Hello, Hamilton Moreno! You have 2 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "id": 2,
+      "isActive": false,
+      "picture": "http://placehold.it/32x32",
+      "name": "Patrica Peterson",
+      "email": "patricapeterson@limage.com",
+      "phone": "+1 (998) 506-2737",
+      "greeting": "Hello, Patrica Peterson! You have 3 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "id": 3,
+      "isActive": false,
+      "picture": "http://placehold.it/32x32",
+      "name": "Louella Grant",
+      "email": "louellagrant@limage.com",
+      "phone": "+1 (952) 427-2873",
+      "greeting": "Hello, Louella Grant! You have 5 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "id": 4,
+      "isActive": false,
+      "picture": "http://placehold.it/32x32",
+      "name": "Lindsey Odom",
+      "email": "lindseyodom@limage.com",
+      "phone": "+1 (887) 542-2607",
+      "greeting": "Hello, Lindsey Odom! You have 10 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "id": 5,
+      "isActive": true,
+      "picture": "http://placehold.it/32x32",
+      "name": "Margarita Miles",
+      "email": "margaritamiles@limage.com",
+      "phone": "+1 (928) 432-3440",
+      "greeting": "Hello, Margarita Miles! You have 6 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "id": 6,
+      "isActive": false,
+      "picture": "http://placehold.it/32x32",
+      "name": "Hardy Cohen",
+      "email": "hardycohen@limage.com",
+      "phone": "+1 (887) 581-3139",
+      "greeting": "Hello, Hardy Cohen! You have 7 unread messages.",
+      "favoriteFruit": "apple"
+    }
+  ]
+}
